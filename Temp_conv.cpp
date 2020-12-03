496a96c49913d1c1c95d60aff9675268f4db867c
Will Poland <75144965+WillPoland@users.noreply.github.com>
2020-12-02 22:02:54 -0600

    Update Temp_conv.cpp

--- Temp_conv.cpp
+++ Temp_conv.cpp
@@ -3,7 +3,7 @@ that read int files*/
 
 //I want to add years
 
-// Change in global Temperature is 0.32*F per year since 1981.
+// Change in global Temperature is 0.18*C per year since 1981.
 
 #include <iostream>
 //lets me use array list.
@@ -87,24 +87,41 @@ int main()
     years.push_back(time);
     cout<<time.getYear()<<endl;
   }
-
+  //locates the file
   ifstream file("1980_temp");
-
+  //intiallize variable
   double celcius;
-
+//print out data written in variable.
   while(file>>celcius)
   {
     cout<<celcius<<endl;
   }
+  //reepeats the block of code 45 times
   for(int i =1; i <= 45;i++)
   {
+    /*adds 0.18 to itself, then add it to celcius
+    the result will be the arguement for setTemp_C()
+    */
     double a = a+ 0.18;
     double result = celcius+a;
     time.setTemp_C(result);
     cout<<time.getTemp_C()<<endl;
   }
+
+  //intialled Farenheit
+  double farenheit = (celcius*(9/5))+32;
+  cout<<farenheit<<endl;
+
+  for(int i =1; i <= 45;i++)
+  {
+    /*adds 0.32 to itself, then add it to farenheit
+    the result will be the arguement for setTemp_F()
+    */
+    double a = a+ 0.32;
+    double result = farenheit+a;
+    time.setTemp_F(result);
+    cout<<time.getTemp_F()<<endl;
+  }
+
   return 0;
 }
-/*Found my problem
-Mr.H, No need to worry!
-It had to do with declaring an object.*/

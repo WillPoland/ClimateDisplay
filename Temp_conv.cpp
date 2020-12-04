8bd13cac42f5fe79e5ec7ee9bb32902315a99c14
Will Poland <75144965+WillPoland@users.noreply.github.com>
2020-12-04 11:50:14 -0600

    Update Temp_conv.cpp

--- Temp_conv.cpp
+++ Temp_conv.cpp
@@ -70,35 +70,29 @@ class Time
 };
 
 
-
-int main()
+void celciusList()
 {
   Time time;
-  //intiallize list of years.
-  //Time is class that the list of year are going through
-  list<Time> years;
-  cout<<"                    AVERAGE GLOBAL TEMPERATURE"<<endl;
-  cout<<"     ________________________________________________________________"<<endl;
-  cout<<"Years"<<endl;
-
-  for(int i = 1980; i <= 2025; i++)
-  {
-    time.setYear(i);
-    years.push_back(time);
-    cout<<time.getYear()<<endl;
-  }
-
-  cout<<"Celcuis"<<endl;
   //locates the file
   ifstream file("1980_temp");
   //intiallize variable
   double celcius;
 //print out data written in variable.
+  double farenheit = (celcius*(9/5))+32;
+
+  list<Time> years;
+
   while(file>>celcius)
   {
+    time.setYear(1980);
+    time.setTemp_F(farenheit);
     time.setTemp_C(celcius);
-    cout<<time.getTemp_C()<<endl;
+    cout<<" "<<time.getYear()<<"   |  "<<time.getTemp_C()<<"   |  "<<time.getTemp_F()<<endl;
+    cout<<"================================="<<endl;
   }
+
+
+
   //reepeats the block of code 45 times
   for(int i =1; i <= 45;i++)
   {
@@ -106,26 +100,29 @@ int main()
     the result will be the arguement for setTemp_C()
     */
     double a = a+ 0.18;
+    double b = b+ 0.32;
     double result = celcius+a;
+    double result1 = farenheit+b;
+    int result2 = 1980+i;
+    time.setYear(result2);
     time.setTemp_C(result);
-    cout<<time.getTemp_C()<<endl;
+    time.setTemp_F(result1);
+    cout<<" "<<time.getYear()<<"   |  "<<time.getTemp_C()<<"   |  "<<time.getTemp_F()<<endl;
+    cout<<"================================="<<endl;
   }
-  cout<<"Farenheit"<<endl;
-  //intialled Farenheit
-  double farenheit = (celcius*(9/5))+32;
-  time.setTemp_F(farenheit);
-  cout<<time.getTemp_F()<<endl;
+}
 
-  for(int i =1; i <= 45;i++)
-  {
-    /*adds 0.32 to itself, then add it to farenheit
-    the result will be the arguement for setTemp_F()
-    */
-    double a = a+ 0.32;
-    double result = farenheit+a;
-    time.setTemp_F(result);
-    cout<<time.getTemp_F()<<endl;
-  }
+int main()
+{
+  Time time;
+
+  cout<<"           AVERAGE GLOBAL TEMPERATURE"<<endl;
+  cout<<"_________________________________"<<endl;
+  cout<<" Years     celcius     Farenheit"<<endl;
+  cout<<"#################################"<<endl;
+
+  cout.precision(5);
+  celciusList();
 
   return 0;
 }
